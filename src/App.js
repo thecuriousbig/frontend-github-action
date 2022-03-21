cafc5594c0e5399b1a861ef18d8c2acf168e45b5
Tanatorn Nateesanprasert <tanatorn.nateesanprasert@gmail.com>
2022-03-21 14:15:08 +0700

    feat: add test

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 2
+          Learn React
         </a>
+        <p>example string</p>
       </header>
     </div>
   );
