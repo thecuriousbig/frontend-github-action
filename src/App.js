08d4797fea4261f7863232124197003874457415
Tanatorn Nateesanprasert <tanatorn.nateesanprasert@gmail.com>
2022-03-24 16:00:02 +0700

    fasda

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log('hello world');
+  console.log('hello main');
   console.log('yay');
   return (
     <div className="App">
